2e976b2426ec9d5ebd04ce1f3183fb50d9032e04
ss12932 <ss12932@my.bristol.ac.uk>
2023-07-28 12:14:28 +0100

    done filter btn + badges + menu cards

---
+++ meal-results-267/src/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const dietaryFilters = ["vegetarian", "protein", "dinner"];
+
+const mealResults = [
+  {
+    mealId: 1,
+    title: "Avocado Sandwich with boiled egg",
+    duration: "15 mins",
+    image: "",
+    tags: ["vegetarian", "protein"],
+    bookmarked: false,
+  },
+  {
+    mealId: 2,
+    title: "Fresh Avocado salad with broccoli",
+    duration: "1 hour",
+    image: "",
+    tags: ["vegetarian", "dinner"],
+  },
+  {
+    mealId: 3,
+    title: "Avocado and spinach pesto sphagetti",
+    duration: "1 hour",
+    image: "",
+    tags: ["vegetarian", "dinner"],
+  },
+];
